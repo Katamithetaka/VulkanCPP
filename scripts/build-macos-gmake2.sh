54dec365bf7f6af0e4ef5918372df55f67527ead
Taka <katamithetaka@gmail.com>
2024-03-22 13:50:27 +0100

    Build Linux

--- scripts/build-macos-gmake2.sh
+++ scripts/build-macos-gmake2.sh
@@ -1,4 +1,4 @@
-workspace_dir=SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+workspace_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 premake=./vendor/premake/macos/premake5
 
 cd $workspace_dir/..
