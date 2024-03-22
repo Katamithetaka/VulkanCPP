aab100f90b7a4c9ecbd40b8e323912426279324e
Taka <katamithetaka@gmail.com>
2024-03-22 14:24:53 +0100

    try to speed up windows build

--- scripts/release.cmd
+++ scripts/release.cmd
@@ -1,14 +1,6 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 
-
-set premake=.\vendor\premake\windows\premake5.exe 
-rem get script directory
-set script_dir=%~dp0
-rem get root directory
-set root_dir=%script_dir%..
-cd %root_dir%
-
-call "%premake%" vs2022
+.\vendor\premake\windows\premake5.exe  vs2022
 
 REM build the solution
 devenv VulkanWorkspace.sln /Build "Release|x64"
