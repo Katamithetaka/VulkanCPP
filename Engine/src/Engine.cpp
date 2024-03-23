990e35b9289211fe4bdd2c913707af6029c53a0d
Taka <katamithetaka@gmail.com>
2024-03-23 09:02:19 +0100

    aaaa

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -1,7 +1,28 @@
 #include "Engine.hpp"
+#include <iostream>
 
 namespace Engine {
+
+
     int add(int a, int b) {
+        VkInstance instance;
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            std::cerr << "Failed to create instance" << std::endl;
+            return -1;
+        }
+
         return a + b;
     }
 }
